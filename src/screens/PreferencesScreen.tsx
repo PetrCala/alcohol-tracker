7f93e57e03bb5ed3d8d228c3a31a4472ec31d0fb
PetrCala <61505008@fsv.cuni.cz>
2023-09-28 20:50:12 +0200

    Modified the preferences screen to enable decimal points input

--- src/screens/PreferencesScreen.tsx
+++ src/screens/PreferencesScreen.tsx
@@ -1,14 +1,11 @@
 ﻿import React, {useContext, useEffect, useRef, useState} from 'react';
 import {
-    Alert,
-  Animated,
+  Alert,
   BackHandler,
   ScrollView,
   StyleSheet,
-  Switch,
   Text,
   TextInput,
-  TouchableOpacity,
   View,
 } from 'react-native';
 import MenuIcon from '../components/Buttons/MenuIcon';
@@ -38,19 +35,28 @@ const PreferencesItem: React.FC<{ item: any }> = ({ item }) => (
 
 interface PreferencesListProps {
   id: string;
-  initialContents: { label: string, value: number }[];
-  onPreferencesChange: (id: string, preferences: { label: string, value: number }[]) => void;
+  initialContents: { label: string, value: string }[];
+  onPreferencesChange: (id: string, preferences: { label: string, value: string }[]) => void;
 }
 
 const PreferencesList: React.FC<PreferencesListProps> = ({ id, initialContents, onPreferencesChange }) => {
   const [localPreferences, setLocalPreferences] = useState(initialContents);
 
   const handleChange = (index: number, text: string) => {
-    let updatedPreferences = [...localPreferences];
-    updatedPreferences[index].value = Number(text);
-    setLocalPreferences(updatedPreferences);
-    onPreferencesChange(id, updatedPreferences);
-  };
+    // Replace comma with point
+    let value = text.replace(',', '.');
+
+    // Validate input
+    const isValid = !isNaN(Number(value)) || value === '.' || /^-?\d*\.\d*$/.test(value);
+    
+    
+    if (isValid) {
+      let updatedPreferences = [...localPreferences];
+      updatedPreferences[index].value = value;
+      setLocalPreferences(updatedPreferences);
+      onPreferencesChange(id, updatedPreferences);
+    }
+};
 
   return (
     <View style={styles.preferencesListContainer}>
@@ -60,8 +66,9 @@ const PreferencesList: React.FC<PreferencesListProps> = ({ id, initialContents, 
           <View style={styles.preferencesListNumericContainer}>
             <TextInput 
               style={styles.preferencesListNumericInput}
-              keyboardType='decimal-pad'
-              maxLength={2}
+              keyboardType='numeric'
+              returnKeyType='default'
+              maxLength={5}
               textAlign='center'
               value={item.value.toString()}
               onChangeText={(text) => handleChange(index, text)}
@@ -83,7 +90,7 @@ const PreferencesScreen = ({ route, navigation }: PreferencesScreenProps) => {
     const { isOnline } = useUserConnection();
     const initialPreferences = useRef(preferences);
     const [showLeaveConfirmation, setShowLeaveConfirmation] = useState(false);
-    const [localListsPreferences, setLocalListsPreferences] = useState<Record<string, { label: string, value: number }[]>>({});
+    const [localListsPreferences, setLocalListsPreferences] = useState<Record<string, { label: string, value: string }[]>>({});
     // Deconstruct the preferences
     const [currentPreferences, setCurrentPreferences] = useState<PreferencesData>({
         first_day_of_week: preferences.first_day_of_week,
@@ -109,20 +116,39 @@ const PreferencesScreen = ({ route, navigation }: PreferencesScreenProps) => {
         }
     };
 
-    const handleListPreferencesChange = (id: string, preferences: { label: string, value: number }[]) => {
+    const handleListPreferencesChange = (id: string, preferences: { label: string, value: string }[]) => {
       setLocalListsPreferences(prev => ({ ...prev, [id]: preferences }));
     };
 
     const handleSavePreferences = async () => {
-        try {
-            await savePreferencesData(db, user.uid, currentPreferences);
-            navigation.navigate("Main Menu Screen", {
-              userData: userData,
-              preferences: currentPreferences
-            });
-        } catch (error:any) {
-            Alert.alert('Preferences saving failed', error.message);
-        };
+      if (areAllValuesValid(localListsPreferences)) {
+          try {
+            console.log('saving preferences...')
+            navigation.goBack();
+              // Somehow get the values and save them, after transforming to numeric
+              // await savePreferencesData(db, user.uid, mergedPreferences);
+              // navigation.navigate("Main Menu Screen", {
+              //     userData: userData,
+              //     preferences: mergedPreferences
+              // });
+          } catch (error:any) {
+              Alert.alert('Preferences saving failed', error.message);
+          };
+      } else {
+          Alert.alert('Invalid Input', 'Please make sure all values are valid numbers.');
+      }
+    };
+    
+    const areAllValuesValid = (listsPreferences: Record<string, { label: string, value: string }[]>) => {
+      for (let id in listsPreferences) {
+          for (let item of listsPreferences[id]) {
+              const value = item.value;
+              if (isNaN(Number(value)) || !/^-?\d*\.?\d*$/.test(value)) {
+                  return false;
+              }
+          }
+      }
+      return true;
     };
 
     const handleFirstDayOfWeekToggle = (value: boolean) => {
@@ -145,10 +171,10 @@ const PreferencesScreen = ({ route, navigation }: PreferencesScreenProps) => {
         label: 'Unit Colors',
         type: 'column',
         contents: <PreferencesList
-          id="unitsToColors"
+          id="units_to_colors"
           initialContents={[
-            {label: 'Yellow', value: currentPreferences.units_to_colors.yellow},
-            {label: 'Orange', value: currentPreferences.units_to_colors.orange}
+            {label: 'Yellow', value: currentPreferences.units_to_colors.yellow.toString()},
+            {label: 'Orange', value: currentPreferences.units_to_colors.orange.toString()}
           ]}
           onPreferencesChange={handleListPreferencesChange}
         />
@@ -157,16 +183,15 @@ const PreferencesScreen = ({ route, navigation }: PreferencesScreenProps) => {
         label: 'Point Conversion',
         type: 'column',
         contents: <PreferencesList
-          id="unitsToPoints" // Another unique identifier
+          id="units_to_points" // Another unique identifier
           initialContents={UnitTypesKeys.map((key, index) => ({
             key: key,
             label: UnitTypesNames[index],
-            value: currentPreferences.units_to_points[key]!  // Non-null assertion
+            value: currentPreferences.units_to_points[key]!.toString()  // Non-null assertion
           }))}
           onPreferencesChange={handleListPreferencesChange}
         />
         },
-        // Add more settings items as needed
     ];
 
     // Make the system back press toggle the go back handler
@@ -187,38 +212,41 @@ const PreferencesScreen = ({ route, navigation }: PreferencesScreenProps) => {
 
     return (
         <View style={{flex:1, backgroundColor: '#FFFF99'}}>
-        <View style={styles.mainHeader}>
-            <MenuIcon
-            iconId='escape-preferences-screen'
-            iconSource={require('../assets/icons/arrow_back.png')}
-            containerStyle={styles.backArrowContainer}
-            iconStyle={styles.backArrow}
-            onPress={handleGoBack}
-            />
-        </View>
-        <ScrollView style={styles.scrollView}>
+          <View style={styles.mainHeader}>
+              <MenuIcon
+              iconId='escape-preferences-screen'
+              iconSource={require('../assets/icons/arrow_back.png')}
+              containerStyle={styles.backArrowContainer}
+              iconStyle={styles.backArrow}
+              onPress={handleGoBack}
+              />
+          </View>
+          <ScrollView 
+            style={styles.scrollView}
+            keyboardShouldPersistTaps="handled"
+          >
             {settingsData.map((item, index) => (
-            <PreferencesItem key={index} item={item} />
-            ))}
-        </ScrollView>
-        <View style={styles.savePreferencesButtonContainer}>
+              <PreferencesItem key={index} item={item} />
+              ))}
+          </ScrollView>
+          <View style={styles.savePreferencesButtonContainer}>
             <BasicButton 
                 text='Save Preferences'
                 buttonStyle={styles.savePreferencesButton}
                 textStyle={styles.savePreferencesButtonText}
                 onPress={handleSavePreferences}
             />
-        </View>
-        <YesNoPopup 
-            visible={showLeaveConfirmation} 
-            transparent={true}
-            onRequestClose={() => setShowLeaveConfirmation(false)}
-            message="You have unsaved changes. Are you sure you want to go back?"
-            onYes={() => {
+          </View>
+          <YesNoPopup 
+              visible={showLeaveConfirmation} 
+              transparent={true}
+              onRequestClose={() => setShowLeaveConfirmation(false)}
+              message="You have unsaved changes. Are you sure you want to go back?"
+              onYes={() => {
                 setShowLeaveConfirmation(false);
                 navigation.goBack();
-            }}
-        />
+              }}
+          />
         </View>
     );
 };
@@ -305,7 +333,7 @@ const styles = StyleSheet.create({
   },
   preferencesListNumericContainer: {
     height: 35,
-    width: 40,
+    width: 60,
     borderRadius: 5,
     backgroundColor: 'white',
     justifyContent: 'center',
@@ -314,19 +342,20 @@ const styles = StyleSheet.create({
     color: 'black'
   },
   preferencesListNumericInput: {
-    height: 35,
-    width: 40,
+    height: 40,
+    width: 60,
     fontSize: 16,
     borderRadius: 5,
-    borderWidth: 2,
+    borderWidth: 1,
     borderColor: '#000',
-    backgroundColor: 'yellow',
+    backgroundColor: '#FFFF99',
     fontWeight: '400',
-    textAlign: 'center'
+    textAlign: 'center',
+    textAlignVertical: 'center',
   },
   savePreferencesButtonContainer: {
     width: '100%',
-    height: '10%',
+    height: 70,
     flexShrink: 1,
     alignItems: "center",
     justifyContent: "center",
@@ -337,7 +366,7 @@ const styles = StyleSheet.create({
   },
   savePreferencesButton: {
     width: '50%',
-    height: '90%',
+    height: 50,
     alignItems: "center",
     justifyContent: 'center',
     padding: 10,
