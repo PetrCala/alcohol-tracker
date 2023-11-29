3e145adc85ed875391ec18ef6b64668b3e992f4a
PetrCala <cala.p@seznam.cz>
2023-11-29 11:44:32 +0100

    Added a short readme to the firebaseConfig test suite for further clarifications down the line

--- __tests__/unit/services/firebaseConfig.test.js
+++ __tests__/unit/services/firebaseConfig.test.js
@@ -75,3 +75,24 @@ describe('Firebase Configuration', () => {
     });
 
 });
+
+
+// ### Understanding Jest's Mocking and Module System
+
+// 1. **Module Caching and Mocks**: When you mock a module in Jest using `jest.mock()`, Jest replaces the module's exports with mock functions or objects as specified in your mock definition. This happens before any test code runs. When you then `require` a module in your test file, you get the mocked version of the module.
+
+// 2. **Why Re-import Inside Each Test**: The reason we re-import the modules (`firebase/app` and `firebase/auth`) inside each test case is due to the behavior of Jest's module caching system. Once a module is required, Jest caches it and subsequent `require` calls return the cached module. If your module reads environment variables at the top level (which is common for configuration modules), it will only read them once, when it's first required. By using `jest.resetModules()` in `beforeEach`, we clear this cache, ensuring that the next `require` call in a test reloads the module afresh, allowing it to read the updated environment variables.
+
+// ### How Does This Not Invalidate the Mocks?
+
+// - When you use `jest.mock()`, Jest replaces the real implementation of the module with your mock both in the module cache and in the module registry. 
+// - The calls to `jest.resetModules()` and re-importing the modules within each test don't invalidate the mocks. They simply ensure that the module with the mocks is re-evaluated (re-run) in the context of the test. 
+// - This means that when you `require` the modules again inside the test, you're still getting the mocked versions, but any top-level code inside those modules (like reading environment variables or performing initial setup) is executed again.
+
+// ### Practical Example:
+
+// - In your `firebaseConfig` file, you might have code that initializes Firebase based on the current environment variables. 
+// - If this initialization code runs only once (the first time the module is imported) and you change environment variables in your tests, the changes won't be picked up unless the module is re-imported.
+// - By resetting the module cache (`jest.resetModules()`) and then re-importing `firebaseConfig` and the mocked modules (`firebase/app` and `firebase/auth`) inside each test, you ensure that the initialization code in `firebaseConfig` runs with the correct, test-specific environment variables.
+
+// In summary, the re-importing of modules after `jest.resetModules()` in each test ensures that your module logic, which may depend on different environment setups, is correctly executed for each test scenario, while still using the mocks defined for external dependencies.
