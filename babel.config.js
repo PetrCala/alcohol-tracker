795b4369c08a35cea68e65ab2d2c2e4508d92ecf
PetrCala <cala.p@seznam.cz>
2023-12-24 22:53:27 +0100

    Updated the babel config file

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,42 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.native.js',
+          '.native.jsx',
+          '.native.ts',
+          '.native.tsx',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.ios.js',
+          '.ios.jsx',
+          '.ios.ts',
+          '.ios.tsx',
+          '.android.js',
+          '.android.jsx',
+          '.android.ts',
+          '.android.tx',
+        ],
+        alias: {
+          '@assets': './assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@libs': './src/libs',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
-// module.exports = function (api) {
-//   api.cache(true);
-//   return {
-//     presets: [
-//       'module:metro-react-native-babel-preset',
-//       ['@babel/preset-env', {targets: {node: 'current'}}],
-//       '@babel/preset-typescript'
-//     ],
-//     plugins: [],
-//   };
-// }
