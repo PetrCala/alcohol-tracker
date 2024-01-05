a04718b658988b0ba81578e8fd84ae820bda8bdf
PetrCala <cala.p@seznam.cz>
2024-01-05 16:02:22 +0100

    WIP on the firebase rules

--- __tests__/integration/emulators/firebaseRules.test.tsx
+++ __tests__/integration/emulators/firebaseRules.test.tsx
@@ -3,61 +3,85 @@
 require('dotenv').config(); // Use .env variables in this file - CONFIG does not work here
 import fs from 'fs';
 import { initializeTestEnvironment, assertFails, assertSucceeds, RulesTestEnvironment, EmulatorConfig } from '@firebase/rules-unit-testing';
-import { describeWithEmulator, shouldRunTests } from "../../utils/emulatorTools";
+import { describeWithEmulator, initializeTestEnv, shouldRunTests, teardownTestEnv } from "../../utils/emulatorTools";
 import * as firebaseRules from '../../../firebase.json';
-import CONST from '@src/CONST';
-
+import { FeedbackProps } from '@src/types/database';
 
 const projectId = process.env.TEST_PROJECT_ID;
 if (!projectId) throw new Error(`Missing environment variable ${projectId}.`);
 
-describeWithEmulator('Test out firebase rules', () => {
+describeWithEmulator('Test firebase rules emulator connection', () => {
     let testEnv: RulesTestEnvironment;
+    let testId: string;
 
     beforeAll(async () => {
-        // can pass null to the auth to have an unauthanticated user
-        const emulatorConfig: EmulatorConfig = {
+        const emulatorConfig = {
             host: 'localhost',
             port: parseInt(firebaseRules.emulators.database.port),
             rules: fs.readFileSync("database.rules.json", "utf8"),
-        }
-        // const myAuth = { uid: "user_id", email: "user@example.com" };
+        };
+
         testEnv = await initializeTestEnvironment({
             projectId: projectId,
             database: emulatorConfig,
-            // storage: //
         });
     });
 
-    // Clean up the emulators after each test
     afterEach(async () => {
         await testEnv.clearDatabase();
-        // await testEnv.clearStorage();
     });
 
     afterAll(async () => {
         await testEnv.clearDatabase();
         await testEnv.cleanup();
     });
-
     it('should succeed with an authentificated user transaction', async () => {
-        expect(1).toBe(1);
-        // const alice = testEnv.authenticatedContext('alice');
-        // await assertSucceeds(alice.database().ref('/private/doc').set({ key: 'value' }));
+        // expect(1).toBe(1);
+        const alice = testEnv.authenticatedContext('alice');
+        await assertSucceeds(alice.database().ref('/private/doc').set({ key: 'value' }));
     });
 
     it('should fail with an unauthentificated user transaction', async () => {
-        expect(1).toBe(1);
-        // const unauthed = testEnv.unauthenticatedContext();
-        // await assertFails(unauthed.database().ref('/private/doc').set({ key: 'value' }));
+        // expect(1).toBe(1);
+        const unauthed = testEnv.unauthenticatedContext();
+        await assertFails(unauthed.database().ref('/private/doc').set({ key: 'value' }));
     });
 
     it('should do something', async () => {
+        // expect(1).toBe(1);
         let testId = 'testId';
         let testDb = testEnv.authenticatedContext(testId).database();
         await assertFails(testDb.ref("/some/path").set({ foo: "bar" }));
         await assertSucceeds(testDb.ref("/another/path").set({ foo: "bar" }));
     });
-
-
 });
+
+// describeWithEmulator('Test feedback rules', () => {
+//     let testEnv: RulesTestEnvironment;
+//     let testId: string;
+
+//     beforeAll(async () => {
+//         testEnv = await initializeTestEnv();
+//     });
+
+//     afterEach(async () => {
+//         await testEnv.clearDatabase();
+//     });
+
+//     afterAll(async () => {
+//         await teardownTestEnv(testEnv);
+//     });
+
+//     it('unauthenticated user can not write feedback', async () => {
+//         const testDb = testEnv.unauthenticatedContext().database();
+//         const testFeedbackId = 'testFeedbackId';
+//         const testFeedback:FeedbackProps = {
+//             submit_time: 0,
+//             text: 'test',
+//             user_id: 'testId',
+//         }
+//         const testRef = testDb.ref(`feedback/${testFeedbackId}`);
+//         await assertFails(testRef.set(testFeedback));
+//     });
+
+// });
--- __tests__/utils/emulatorTools.tsx
+++ __tests__/utils/emulatorTools.tsx
@@ -1,6 +1,34 @@
 require('dotenv').config(); // for the process.env variables to read the .env file
+import fs from 'fs';
+import { initializeTestEnvironment, RulesTestEnvironment } from '@firebase/rules-unit-testing';
+import * as firebaseRules from '../../firebase.json';
+
 import CONST from '@src/CONST';
 
 // Perhaps if this grows too largs, rewrite into a module export
 export const shouldRunTests = process.env.APP_ENVIRONMENT === CONST.ENVIRONMENT.TEST;
 export const describeWithEmulator = shouldRunTests ? describe : describe.skip;
+
+
+export async function initializeTestEnv():Promise<RulesTestEnvironment> {
+    const projectId = process.env.TEST_PROJECT_ID;
+    if (!projectId) {
+        throw new Error('Missing environment variable TEST_PROJECT_ID.');
+    }
+
+    const emulatorConfig = {
+        host: 'localhost',
+        port: parseInt(firebaseRules.emulators.database.port),
+        rules: fs.readFileSync("database.rules.json", "utf8"),
+    };
+
+    return await initializeTestEnvironment({
+        projectId: projectId,
+        database: emulatorConfig,
+    });
+}
+
+export async function teardownTestEnv(testEnv: RulesTestEnvironment) {
+    await testEnv.clearDatabase();
+    await testEnv.cleanup();
+};
