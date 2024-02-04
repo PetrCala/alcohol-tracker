6e6165de26bf993cfd205c8bb537209bf5f9e552
PetrCala <cala.p@seznam.cz>
2024-02-04 21:35:28 +0100

    Improved the user overview FE majorly

--- src/components/Social/UserOverview.tsx
+++ src/components/Social/UserOverview.tsx
@@ -1,10 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import {ProfileData, UserStatusData} from '../../types/database';
 import {useFirebase} from '../../context/global/FirebaseContext';
 import ProfileImage from '@components/ProfileImage';
 import {getTimestampAge, isRecent} from '@src/utils/timeUtils';
 import SuccessIndicator from '@components/SuccessIndicator';
 import commonStyles from '@src/styles/commonStyles';
+import {sumAllUnits} from '@src/utils/dataHandling';
 
 type UserOverviewProps = {
   userId: string;
@@ -25,19 +26,31 @@ const UserOverview: React.FC<UserOverviewProps> = ({
   const {last_online, latest_session, latest_session_id} = userStatusData;
   const activeNow = isRecent(last_online);
   const lastSeen = getTimestampAge(last_online);
+  const inSession = latest_session?.ongoing;
+  const unitsThisSession = latest_session
+    ? sumAllUnits(latest_session?.units)
+    : null;
 
   return (
     <View key={userId + '-container'} style={styles.userOverviewContainer}>
       <View key={userId + '-left-container'} style={styles.leftContainer}>
         <View key={userId + '-profile'} style={styles.userOverviewProfile}>
-          <ProfileImage
-            key={userId + '-profile-icon'}
-            storage={storage}
-            userId={userId}
-            downloadPath={profileData.photo_url}
-            style={styles.userOverviewImage}
-          />
-          <View key={userId + 'info'} style={styles.userInfoContainer}>
+          <View style={styles.imageContainer}>
+            <ProfileImage
+              key={userId + '-profile-icon'}
+              storage={storage}
+              userId={userId}
+              downloadPath={profileData.photo_url}
+              style={styles.userOverviewImage}
+            />
+          </View>
+          <View
+            key={userId + 'info'}
+            style={
+              inSession
+                ? styles.userInfoContainer
+                : [styles.userInfoContainer, styles.centerUserInfo]
+            }>
             <Text
               key={userId + '-nickname'}
               style={[styles.userOverviewText, {flexShrink: 1}]}
@@ -45,11 +58,20 @@ const UserOverview: React.FC<UserOverviewProps> = ({
               ellipsizeMode="tail">
               {profileData.display_name}
             </Text>
-            <Text
-              key={userId + '-sessions'}
-              style={[styles.userDetailsText, styles.leftContainerText]}>
-              some info
-            </Text>
+            {inSession && (
+              <Text
+                key={userId + '-sessions'}
+                style={[styles.userDetailsText, styles.leftContainerText]}>
+                Currently in session
+              </Text>
+            )}
+            {inSession && unitsThisSession && (
+              <Text
+                key={userId + '-units'}
+                style={[styles.userDetailsText, styles.leftContainerText]}>
+                Units so far: {unitsThisSession}
+              </Text>
+            )}
           </View>
         </View>
       </View>
@@ -85,18 +107,22 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     width: '30%',
     height: '100%',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
     alignItems: 'flex-end',
     padding: 5,
-    // backgroundColor: 'pink',
   },
   userOverviewProfile: {
-    width: '60%',
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    padding: 5,
     paddingTop: 7,
+  },
+  imageContainer: {
+    width: '30%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   userOverviewImage: {
     width: 70,
@@ -105,24 +131,31 @@ const styles = StyleSheet.create({
     borderRadius: 35,
   },
   userInfoContainer: {
+    height: '100%',
+    width: '70%',
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    padding: 5,
+    marginTop: 5,
+  },
+  centerUserInfo: {
+    justifyContent: 'center',
+    marginTop: 0,
   },
   userOverviewText: {
     color: 'black',
     fontSize: 16,
     fontWeight: '500',
-    marginLeft: 10,
+    marginLeft: 5,
+    marginBottom: 3,
   },
   userDetailsText: {
     color: 'black',
     fontSize: 12,
   },
   leftContainerText: {
-    marginLeft: 20,
-    marginTop: 5,
+    marginLeft: 15,
+    margin: 2,
   },
   rightContainerText: {
     margin: 5,
