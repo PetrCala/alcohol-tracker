2cecafd89b30717d377d3331b3b847943feb0e74
PetrCala <cala.p@seznam.cz>
2024-02-04 21:44:26 +0100

    Improved the timestamp age logic

--- src/utils/timeUtils.tsx
+++ src/utils/timeUtils.tsx
@@ -1,3 +1,5 @@
+import {format} from 'path';
+
 export function isRecent(timestamp: number): boolean {
   const now = Date.now();
   const fiveMinutes = 5 * 60 * 1000; // 5 minutes in milliseconds
@@ -7,29 +9,32 @@ export function isRecent(timestamp: number): boolean {
 export function getTimestampAge(timestamp: number): string {
   const now = Date.now();
   const difference = now - timestamp;
+  const plural = (input: number) => (input > 1 ? 's' : '');
+  const formatText = (input: number, unit: string) =>
+    `${input} ${unit}${plural(input)} ago`;
 
   // Format the timestamp into human-readable form based on its age
   switch (true) {
     case difference < 60 * 1000:
       const seconds = Math.floor(difference / 1000);
-      return `${seconds} seconds ago`;
+      return formatText(seconds, 'second');
     case difference < 60 * 60 * 1000:
       const minutes = Math.floor(difference / (60 * 1000));
-      return `${minutes} minutes ago`;
+      return formatText(minutes, 'minute');
     case difference < 24 * 60 * 60 * 1000:
       const hours = Math.floor(difference / (60 * 60 * 1000));
-      return `${hours} hours ago`;
+      return formatText(hours, 'hour');
     case difference < 7 * 24 * 60 * 60 * 1000:
       const days = Math.floor(difference / (24 * 60 * 60 * 1000));
-      return `${days} days ago`;
+      return formatText(days, 'day');
     case difference < 30 * 24 * 60 * 60 * 1000:
       const weeks = Math.floor(difference / (7 * 24 * 60 * 60 * 1000));
-      return `${weeks} weeks ago`;
+      return formatText(weeks, 'week');
     case difference < 365 * 24 * 60 * 60 * 1000:
       const months = Math.floor(difference / (30 * 24 * 60 * 60 * 1000));
-      return `${months} months ago`;
+      return formatText(months, 'month');
     default:
       const years = Math.floor(difference / (365 * 24 * 60 * 60 * 1000));
-      return `${years} years ago`;
+      return formatText(years, 'year');
   }
 }
