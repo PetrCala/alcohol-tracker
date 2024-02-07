01211ec83fe68501a89c1dc3ee379212a5941046
PetrCala <cala.p@seznam.cz>
2024-02-07 22:27:09 +0100

    Added a small margin for the main screen units overview

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -464,6 +464,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#FFFF99',
     width: '100%',
+    marginTop: 2,
   },
   menuInfoItemContainer: {
     flexDirection: 'row',
