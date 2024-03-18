df0fdfe423f2dd25c79a95792972258266cbe5ca
PetrCala <cala.p@seznam.cz>
2024-03-18 14:10:53 +0100

    Added a QR code to the markdown

--- src/components/Icon/KirokuImages.ts
+++ src/components/Icon/KirokuImages.ts
@@ -1,5 +1,7 @@
 import Logo from '@assets/images/app-logo.png';
 import UnderMaintenance from '@assets/images/under-maintenance.jpg';
 import UserIcon from '@assets/temp/user-icon.png';
+import QrCodePlain from '@assets/images/qr-code-plain.png';
+import QrCodeWithLogo from '@assets/images/qr-code-with-logo.png';
 
-export {Logo, UnderMaintenance, UserIcon};
+export {Logo, UnderMaintenance, UserIcon, QrCodePlain, QrCodeWithLogo};
