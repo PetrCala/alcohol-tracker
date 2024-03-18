da72a6c5ca43265efa79591b0bbbed5e0d3c31c6
PetrCala <cala.p@seznam.cz>
2024-03-18 13:37:18 +0100

    Added a .html featuring application download links

--- src/components/Icon/KirokuImages.ts
+++ src/components/Icon/KirokuImages.ts
@@ -1,4 +1,4 @@
-import Logo from '@assets/logo/alcohol-tracker-source-icon.png';
+import Logo from '@assets/images/app-logo.png';
 import UnderMaintenance from '@assets/images/under-maintenance.jpg';
 import UserIcon from '@assets/temp/user-icon.png';
 
