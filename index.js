b456fa7e578b78111ee5267a2ecf98cbb284b43f
PetrCala <cala.p@seznam.cz>
2024-03-27 18:16:15 +0100

    Minor build changes to android

--- index.js
+++ index.js
@@ -5,10 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from '@src/App';
 import CONFIG from '@src/CONFIG';
-console.log(
-  'registering the app component under name',
-  CONFIG.COMPONENT_NAME,
-  '...',
-);
 
 AppRegistry.registerComponent(CONFIG.COMPONENT_NAME, () => App);
