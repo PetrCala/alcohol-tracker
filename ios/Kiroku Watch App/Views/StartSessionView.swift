4cdcd4de986067d1c2a8eb112ac4552b471bffe9
PetrCala <cala.p@seznam.cz>
2024-07-07 19:02:01 +0200

    Enlarged the tappable area on the start session screen

--- ios/Kiroku Watch App/Views/StartSessionView.swift
+++ ios/Kiroku Watch App/Views/StartSessionView.swift
@@ -8,20 +8,24 @@ struct StartSessionView: View {
             Button(action: {
                 SessionModel.shared.startSession()
             }) {
-                Image("AppImage")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle()) // Make the image round
-                    // .overlay(Circle().stroke(AppColors.contrastColor, lineWidth: 4)) // Optional: Add a white border around the image
+                VStack {
+                    Image("AppImage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle()) // Make the image round
+                        // .overlay(Circle().stroke(AppColors.contrastColor, lineWidth: 4)) // Optional: Add a white border around the image
+
+                    Text("Tap to Start Session")
+                        .font(.caption) // Smaller text
+                        .foregroundColor(.white)
+                        .padding(.top, 8)
+                }
             }
+            .frame(width: 200, height: 200) // Match the button size to the image size plus text height
+            .cornerRadius(100) // Round the corners of the button to match the circle image
             .buttonStyle(PlainButtonStyle()) // Make the button invisible
     
-            Text("Tap to Start Session")
-                .font(.caption) // Smaller text
-                .foregroundColor(.white)
-                .padding(.top, 8)
-            
             Spacer()
         }
     }
