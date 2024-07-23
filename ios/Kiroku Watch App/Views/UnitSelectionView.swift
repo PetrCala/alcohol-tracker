7106b0cdd4ea025a3755b6f8cf5282420f3ea06a
PetrCala <cala.p@seznam.cz>
2024-07-23 22:34:06 +0200

    Break down the unit selection view into two components to avoid a compilation error

--- ios/Kiroku Watch App/Views/UnitSelectionView.swift
+++ ios/Kiroku Watch App/Views/UnitSelectionView.swift
@@ -1,8 +1,78 @@
 import SwiftUI
 
+struct UnitView: View {
+    let unit: UnitModel
+    let isSelected: Bool
+    let outerGeometry: GeometryProxy
+    let innerGeometry: GeometryProxy
+    let frameHeight: CGFloat
+    let updateSelectedUnit: (GeometryProxy, GeometryProxy, Int) -> Void
+    
+    var body: some View {
+        HStack {
+            Text(unit.unitName)
+                .font(.body)
+            
+            Spacer()
+            
+            // Image(systemName: "checkmark")
+            Image(unit.unitImageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: frameHeight / 2, height: frameHeight / 2)
+                .renderingMode(.template)
+                .foregroundColor(AppColors.contrastColor.opacity(0.1))
+        }
+        .frame(width: outerGeometry.size.width - 32)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(isSelected ? AppColors.primaryColor : Color.gray)
+                .background(isSelected ? AppColors.primaryColor.opacity(0.1) : Color.clear)
+        )
+        .onAppear {
+            updateSelectedUnit(innerGeometry, outerGeometry, unit.unitId)
+        }
+        .onChange(of: innerGeometry.frame(in: .global).midY) { _ in
+            updateSelectedUnit(innerGeometry, outerGeometry, unit.unitId)
+        }
+        .frame(height: frameHeight)
+    }
+}
+
 struct UnitSelectionView: View {
     @State private var selectedUnitId: Int? = nil
     let units: [UnitModel] = UnitModel.getUnits()
+    let frameHeight: CGFloat = 60
+
+    var body: some View {
+            GeometryReader { outerGeometry in
+                ScrollView {
+                    LazyVStack(spacing: 0) {
+                        ForEach(units, id: \.unitId) { unit in
+                            GeometryReader { innerGeometry in
+                                UnitView(
+                                    unit: unit,
+                                    isSelected: selectedUnitId == unit.unitId,
+                                    outerGeometry: outerGeometry,
+                                    innerGeometry: innerGeometry,
+                                    frameHeight: frameHeight,
+                                    updateSelectedUnit: updateSelectedUnit
+                                )
+                                .frame(height: frameHeight)
+                            }
+                            .frame(height: frameHeight)
+                        }
+                    }
+                    .padding()
+
+                    // Add space below the stack for the last item
+                    // to be centered when it's selected
+                    Spacer().frame(height: outerGeometry.size.height / 2)
+                }
+            }
+        }
+
 
     private func updateSelectedUnit(innerGeometry: GeometryProxy, outerGeometry: GeometryProxy, unitId: Int) {
         let globalCenterY = innerGeometry.frame(in: .global).midY
@@ -15,51 +85,6 @@ struct UnitSelectionView: View {
         }
     }
 
-    var body: some View {
-            GeometryReader { outerGeometry in
-                ScrollView {
-                    LazyVStack(spacing: 0) {
-                        ForEach(units, id: \.unitId) { unit in
-                            GeometryReader { innerGeometry in
-                                HStack {
-                                    Text(unit.unitName)
-                                        .font(.body)
-                                    
-                                    Spacer()
-                                    
-                                    // Image(systemName: "checkmark")
-                                    Image(unit.unitImageName)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 30, height: 30)
-                                        .renderingMode(.template)
-                                        .foregroundColor(AppColors.contrastColor.opacity(0.1))
-                                }
-                                .frame(width: outerGeometry.size.width - 32)
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(selectedUnitId == unit.unitId ? AppColors.primaryColor : Color.gray)
-                                        .background(selectedUnitId == unit.unitId ? AppColors.primaryColor.opacity(0.1) : Color.clear)
-                                )
-                                .onAppear {
-                                    updateSelectedUnit(innerGeometry: innerGeometry, outerGeometry: outerGeometry, unitId: unit.unitId)
-                                }
-                                .onChange(of: innerGeometry.frame(in: .global).midY) { _ in
-                                    updateSelectedUnit(innerGeometry: innerGeometry, outerGeometry: outerGeometry, unitId: unit.unitId)
-                                }
-                            }
-                            .frame(height: 60) // Adjust the height as necessary
-                        }
-                    }
-                    .padding()
-
-                    // Add space below the stack for the last item
-                    //  to be centered when it's selected
-                    Spacer().frame(height: outerGeometry.size.height / 2)
-                }
-            }
-        }
 }
 
 struct UnitSelectionView_Previews: PreviewProvider {
