c4e6bd18038196d3293aeb602d43ae23809ae5a6
PetrCala <cala.p@seznam.cz>
2024-09-04 11:16:21 +0200

    Remove react-native-haptic-feedback

--- src/libs/HapticFeedback/index.native.ts
+++ src/libs/HapticFeedback/index.native.ts
@@ -1,27 +1,43 @@
-import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 import type HapticFeedback from './types';
 
+/**
+ * Web does not support Haptic feedback
+ */
 const hapticFeedback: HapticFeedback = {
-  press: () => {
-    ReactNativeHapticFeedback.trigger('impactLight', {
-      enableVibrateFallback: true,
-    });
-  },
-  longPress: () => {
-    ReactNativeHapticFeedback.trigger('impactHeavy', {
-      enableVibrateFallback: true,
-    });
-  },
-  success: () => {
-    ReactNativeHapticFeedback.trigger('notificationSuccess', {
-      enableVibrateFallback: true,
-    });
-  },
-  error: () => {
-    ReactNativeHapticFeedback.trigger('notificationError', {
-      enableVibrateFallback: true,
-    });
-  },
+  press: () => {},
+  longPress: () => {},
+  success: () => {},
+  error: () => {},
 };
 
 export default hapticFeedback;
+
+// The react-native-haptic-feedback package had broken compilation on iOS for version 2.2.0, so we removed it. Otherwise, this would be the native implementation
+
+// import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+// import type HapticFeedback from './types';
+
+// const hapticFeedback: HapticFeedback = {
+//   press: () => {
+//     ReactNativeHapticFeedback.trigger('impactLight', {
+//       enableVibrateFallback: true,
+//     });
+//   },
+//   longPress: () => {
+//     ReactNativeHapticFeedback.trigger('impactHeavy', {
+//       enableVibrateFallback: true,
+//     });
+//   },
+//   success: () => {
+//     ReactNativeHapticFeedback.trigger('notificationSuccess', {
+//       enableVibrateFallback: true,
+//     });
+//   },
+//   error: () => {
+//     ReactNativeHapticFeedback.trigger('notificationError', {
+//       enableVibrateFallback: true,
+//     });
+//   },
+// };
+
+// export default hapticFeedback;
