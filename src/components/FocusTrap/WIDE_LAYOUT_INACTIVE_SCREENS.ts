9378301bdbb73b40529c5d8d43be0a11b60b19ae
PetrCala <cala.p@seznam.cz>
2024-09-15 12:49:06 +0200

    Add the FocusTrap component

---
+++ src/components/FocusTrap/WIDE_LAYOUT_INACTIVE_SCREENS.ts
@@ -0,0 +1,15 @@
+import NAVIGATORS from '@src/NAVIGATORS';
+import SCREENS from '@src/SCREENS';
+
+/**
+ * Screens displayed in the BottomTab and CentralPane displayed side by side that should not have active
+ * focus trap when rendered on a wide screen to allow navigation between them using the keyboard
+ */
+const WIDE_LAYOUT_INACTIVE_SCREENS: string[] = [
+  NAVIGATORS.BOTTOM_TAB_NAVIGATOR,
+  SCREENS.HOME,
+  SCREENS.SETTINGS.ROOT,
+  // ...
+];
+
+export default WIDE_LAYOUT_INACTIVE_SCREENS;
