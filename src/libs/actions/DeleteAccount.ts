eff45583eba18589a29919c1e8cd17aece4932e6
PetrCala <cala.p@seznam.cz>
2024-10-31 08:41:10 +0900

    The account deletion now navigates to the login screen automatically through auth state change

--- src/libs/actions/DeleteAccount.ts
+++ src/libs/actions/DeleteAccount.ts
@@ -63,7 +63,7 @@ async function deleteAccount(
     await signOut(auth);
 
     // Add an alert here informing about the user deletion
-    Navigation.navigate(ROUTES.LOGIN);
+    // Navigation.navigate(ROUTES.LOGIN); // This is has been disabled as the redirect happens automatically upon Auth state change
   } catch (error: any) {
     ErrorUtils.raiseAlert(
       error,
