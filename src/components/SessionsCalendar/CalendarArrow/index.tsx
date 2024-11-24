8fa08aef8ec0966cdd0b8101ec23bea436a731eb
PetrCala <cala.p@seznam.cz>
2024-11-24 10:39:50 +0100

    Remove unused imports

--- src/components/SessionsCalendar/CalendarArrow/index.tsx
+++ src/components/SessionsCalendar/CalendarArrow/index.tsx
@@ -1,13 +1,8 @@
-import Icon from '@components/Icon';
-import useTheme from '@hooks/useTheme';
-import * as KirokuIcons from '@components/Icon/KirokuIcons';
-import useWindowDimensions from '@hooks/useWindowDimensions';
-import {StyleSheet, View} from 'react-native';
+import {View} from 'react-native';
 import useThemeStyles from '@hooks/useThemeStyles';
 import ArrowIcon from '@components/DatePicker/CalendarPicker/ArrowIcon';
 import CONST from '@src/CONST';
 import {ValueOf} from 'type-fest';
-import variables from '@src/styles/variables';
 
 /** Direction of the calendar arrow */
 type Direction = ValueOf<typeof CONST.DIRECTION>;
