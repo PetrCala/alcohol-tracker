77d6caf6efc2c3f6a06e30756bc23de99edcd335
PetrCala <cala.p@seznam.cz>
2024-11-27 16:09:11 +0100

    The verify email screen now shows a success animation upon successful verification

--- src/screens/VerifyEmailScreen.tsx
+++ src/screens/VerifyEmailScreen.tsx
@@ -18,23 +18,24 @@ import Onyx from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 import Icon from '@components/Icon';
 import useTheme from '@hooks/useTheme';
+import SuccessAnimation from '@components/SuccessAnimation';
+import {sleep} from '@libs/TimeUtils';
 
 type VerifyEmailScreenOnyxProps = {};
 type VerifyEmailScreenProps = VerifyEmailScreenOnyxProps &
   StackScreenProps<AuthScreensParamList, typeof SCREENS.VERIFY_EMAIL>;
 
 function VerifyEmailScreen({route}: VerifyEmailScreenProps) {
+  const user = auth.currentUser;
   const styles = useThemeStyles();
   const {translate} = useLocalize();
   const theme = useTheme();
   const [emailSent, setEmailSent] = useState(false);
+  const [emailVerified, setEmailVerified] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [errorText, setErrorText] = useState('');
-  const [mockEmailVerified, setMockEmailVerified] = useState(false); // TODO delete
-  const user = auth.currentUser;
 
   const onVerifyEmailButtonPress = async () => {
-    // setMockEmailVerified(true); // TODO delete
     try {
       setErrorText('');
       setIsLoading(true);
@@ -44,6 +45,18 @@ function VerifyEmailScreen({route}: VerifyEmailScreenProps) {
       setErrorText(error.message);
     } finally {
       setIsLoading(false);
+    }
+  };
+
+  const onChangeEmailButtonPress = () => {
+    if (!emailSent) {
+      Navigation.navigate(ROUTES.SETTINGS_EMAIL);
+      return;
+    }
+    if (user && user?.emailVerified) {
+      setEmailVerified(true);
+    } else {
+      setErrorText(translate('verifyEmailScreen.error.emailNotVerified'));
     }
   };
 
@@ -57,14 +70,16 @@ function VerifyEmailScreen({route}: VerifyEmailScreenProps) {
       });
   };
 
+  const onSuccessAnimationEnd = async () => {
+    await sleep(1000).then(() => {
+      Navigation.navigate(ROUTES.HOME);
+    });
+  };
+
   // Redirect to home screen if user is verified
   useEffect(() => {
-    if (mockEmailVerified) {
-      Navigation.navigate(ROUTES.HOME);
-      return;
-    }
     if (user && user.emailVerified) {
-      Navigation.navigate(ROUTES.HOME);
+      setEmailVerified(true);
     }
   }, [user]);
 
@@ -73,69 +88,87 @@ function VerifyEmailScreen({route}: VerifyEmailScreenProps) {
       testID={VerifyEmailScreen.displayName}
       style={styles.appContent}>
       <View style={[styles.flex1, styles.ph4]}>
-        <View
-          style={[
-            styles.flexGrow1,
-            styles.justifyContentCenter,
-            styles.alignItemsCenter,
-            styles.mh4,
-          ]}>
-          <Icon src={KirokuIcons.Mail} fill={theme.appColor} large />
-          <Text textAlign="center" style={[styles.textHeadlineH2, styles.mt3]}>
-            {translate('verifyEmailScreen.youAreNotVerified')}
-          </Text>
-          <Text textAlign="center" style={styles.mt3}>
-            {translate(
-              'verifyEmailScreen.wouldYouLikeToVerify',
-              user?.email ?? '',
-            )}
-          </Text>
-        </View>
-        <View style={styles.pb1}>
-          {!!emailSent && !errorText && (
-            <DotIndicatorMessage
-              style={[styles.mv2]}
-              type="success"
-              // eslint-disable-next-line @typescript-eslint/naming-convention,@typescript-eslint/prefer-nullish-coalescing
-              messages={{
-                0: translate('verifyEmailScreen.emailSent'),
-              }}
-            />
-          )}
-          {!!errorText && (
-            <DotIndicatorMessage
-              style={[styles.mv2]}
-              type="error"
-              // eslint-disable-next-line @typescript-eslint/naming-convention,@typescript-eslint/prefer-nullish-coalescing
-              messages={{0: errorText || ''}}
-            />
-          )}
-          <Button
-            success
-            isLoading={isLoading}
-            style={styles.mt1}
-            text={translate(
-              emailSent
-                ? 'verifyEmailScreen.resendEmail'
-                : 'verifyEmailScreen.verifyEmail',
-            )}
-            onPress={onVerifyEmailButtonPress}
-            large
+        {!emailVerified ? (
+          <>
+            <View
+              style={[
+                styles.flexGrow1,
+                styles.justifyContentCenter,
+                styles.alignItemsCenter,
+                styles.mh4,
+              ]}>
+              <Icon src={KirokuIcons.Mail} fill={theme.appColor} large />
+              <Text
+                textAlign="center"
+                style={[styles.textHeadlineH2, styles.mt3]}>
+                {translate('verifyEmailScreen.youAreNotVerified')}
+              </Text>
+              <Text textAlign="center" style={styles.mt3}>
+                {translate(
+                  'verifyEmailScreen.wouldYouLikeToVerify',
+                  user?.email ?? '',
+                )}
+              </Text>
+            </View>
+            <View style={styles.pb1}>
+              {!!emailSent && !errorText && (
+                <DotIndicatorMessage
+                  style={[styles.mv2]}
+                  type="success"
+                  // eslint-disable-next-line @typescript-eslint/naming-convention,@typescript-eslint/prefer-nullish-coalescing
+                  messages={{
+                    0: translate('verifyEmailScreen.emailSent'),
+                  }}
+                />
+              )}
+              {!!errorText && (
+                <DotIndicatorMessage
+                  style={[styles.mv2]}
+                  type="error"
+                  // eslint-disable-next-line @typescript-eslint/naming-convention,@typescript-eslint/prefer-nullish-coalescing
+                  messages={{0: errorText || ''}}
+                />
+              )}
+              <Button
+                success
+                isLoading={isLoading}
+                style={styles.mt1}
+                text={translate(
+                  emailSent
+                    ? 'verifyEmailScreen.resendEmail'
+                    : 'verifyEmailScreen.verifyEmail',
+                )}
+                onPress={onVerifyEmailButtonPress}
+                large
+              />
+              <Button
+                style={[styles.mt1]}
+                text={translate(
+                  emailSent
+                    ? 'verifyEmailScreen.iHaveVerified'
+                    : 'verifyEmailScreen.changeEmail',
+                )}
+                onPress={onChangeEmailButtonPress}
+                large
+              />
+              <Button
+                text={translate('verifyEmailScreen.illDoItLater')}
+                style={styles.bgTransparent}
+                textStyles={styles.textAppColor}
+                onPress={onDismissVerifyEmail}
+                large
+              />
+            </View>
+          </>
+        ) : (
+          <SuccessAnimation
+            iconSource={KirokuIcons.Checkmark}
+            text={translate('verifyEmailScreen.emailVerified')}
+            visible={true}
+            onAnimationEnd={onSuccessAnimationEnd}
+            style={styles.flexGrow1}
           />
-          <Button
-            style={[styles.mt1]}
-            text={translate('verifyEmailScreen.changeEmail')}
-            onPress={() => Navigation.navigate(ROUTES.SETTINGS_EMAIL)}
-            large
-          />
-          <Button
-            text={translate('verifyEmailScreen.illDoItLater')}
-            style={styles.bgTransparent}
-            textStyles={styles.textAppColor}
-            onPress={onDismissVerifyEmail}
-            large
-          />
-        </View>
+        )}
       </View>
     </ScreenWrapper>
   );
