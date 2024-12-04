4b3d0e6b23b747d42486af73316ef8f1d095c607
PetrCala <cala.p@seznam.cz>
2024-12-04 13:48:28 +0100

    Minor lint issues fix

--- src/types/onyx/UserStatus.ts
+++ src/types/onyx/UserStatus.ts
@@ -2,8 +2,13 @@ import type {UserID} from './OnyxCommon';
 import type DrinkingSession from './DrinkingSession';
 
 type UserStatus = {
+  /** A timestamp indicating the last time a user was online */
   last_online: number;
+
+  /** ID of the latest drinking session */
   latest_session_id?: string | null;
+
+  /** The user's latest drinking session data */
   latest_session?: DrinkingSession | null;
 };
 
