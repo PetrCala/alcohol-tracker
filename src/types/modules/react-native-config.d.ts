e20078d3b650f0da863e3289cb78e0b35e78a9c2
PetrCala <cala.p@seznam.cz>
2024-12-04 14:05:08 +0100

    Fix a lint error in the react native config types file

--- src/types/modules/react-native-config.d.ts
+++ src/types/modules/react-native-config.d.ts
@@ -1,23 +1,22 @@
 import type CONST from '@src/CONST';
 import type DeepValueOf from '@src/types/utils/DeepValueOf';
 
-declare module 'react-native-config' {
-  export type NativeConfig = {
-    ENVIRONMENT: DeepValueOf<typeof CONST.ENVIRONMENT>;
-    API_KEY: string;
-    AUTH_DOMAIN: string;
-    DATABASE_URL: string;
-    PROJECT_ID: string;
-    STORAGE_BUCKET: string;
-    MESSAGING_SENDER_ID: number;
-    APP_ID: string;
-    MEASUREMENT_ID: string;
-    PUSHER_APP_KEY: string;
-    PUSHER_DEV_SUFFIX: string;
-    USE_WEB_PROXY?: string;
-    SEND_CRASH_REPORTS?: string;
-  };
+export type NativeConfig = {
+  ENVIRONMENT: DeepValueOf<typeof CONST.ENVIRONMENT>;
+  API_KEY: string;
+  AUTH_DOMAIN: string;
+  DATABASE_URL: string;
+  PROJECT_ID: string;
+  STORAGE_BUCKET: string;
+  MESSAGING_SENDER_ID: number;
+  APP_ID: string;
+  MEASUREMENT_ID: string;
+  PUSHER_APP_KEY: string;
+  PUSHER_DEV_SUFFIX: string;
+  USE_WEB_PROXY?: string;
+  SEND_CRASH_REPORTS?: string;
+};
 
-  export const Config: NativeConfig;
-  export default Config;
-}
+declare const Config: NativeConfig;
+
+export default Config;
