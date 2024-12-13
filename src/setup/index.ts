73332b55958435fe29787dbaa1fa3f3d4e441dff
PetrCala <cala.p@seznam.cz>
2024-12-13 18:32:19 +0100

    Fix various lint issues

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -30,7 +30,6 @@ export default function () {
     initialKeyStates: {
       // Clear any loading and error messages so they do not appear on app startup
       [ONYXKEYS.SESSION]: {loading: false},
-      [ONYXKEYS.FORMS.CLOSE_ACCOUNT_FORM]: CONST.DEFAULT_CLOSE_ACCOUNT_DATA,
       [ONYXKEYS.NETWORK]: CONST.DEFAULT_NETWORK_DATA,
       [ONYXKEYS.IS_SIDEBAR_LOADED]: false,
       [ONYXKEYS.SESSIONS_CALENDAR_MONTHS_LOADED]: 0, // Reset calendar
