44d1dc1fe1c970192f4967688da524ed17955fa1
PetrCala <cala.p@seznam.cz>
2024-12-23 14:47:08 +0100

    Add various module types to the global RN config file

--- src/types/modules/react-native.d.ts
+++ src/types/modules/react-native.d.ts
@@ -14,7 +14,10 @@ import type {
   WheelEventHandler,
 } from 'react';
 import 'react-native';
+import type {TargetedEvent} from 'react-native';
 import type {BootSplashModule} from '@libs/BootSplash/types';
+import type {EnvironmentCheckerModule} from '@libs/Environment/betaChecker/types';
+import type {ShortcutManagerModule} from '@libs/ShortcutManager';
 import type StartupTimer from '@libs/StartupTimer/types';
 
 type HybridAppModule = {
@@ -24,7 +27,57 @@ type HybridAppModule = {
   exitApp: () => void;
 };
 
+type RNTextInputResetModule = {
+  resetKeyboardInput: (nodeHandle: number | null) => void;
+};
+
+type RNNavBarManagerModule = {
+  setButtonStyle: (style: 'light' | 'dark') => void;
+};
+
 declare module 'react-native' {
+  interface TextInputFocusEventData extends TargetedEvent {
+    text: string;
+    eventCount: number;
+    relatedTarget?: {
+      id?: string;
+    };
+  }
+
+  interface PressableStateCallbackType extends WebPressableStateCallbackType {
+    readonly isScreenReaderActive: boolean;
+    readonly isDisabled: boolean;
+  }
+
+  interface AppStateStatic {
+    emitCurrentTestState: (status: string) => void;
+  }
+
+  interface LinkingStatic {
+    setInitialURL: (url: string) => void;
+  }
+
+  interface NativeModulesStatic {
+    BootSplash: BootSplashModule;
+    HybridAppModule: HybridAppModule;
+    StartupTimer: StartupTimer;
+    RNTextInputReset: RNTextInputResetModule;
+    RNNavBarManager: RNNavBarManagerModule;
+    EnvironmentChecker: EnvironmentCheckerModule;
+    ShortcutManager: ShortcutManagerModule;
+  }
+
+  namespace Animated {
+    interface AnimatedInterpolation<OutputT extends number | string>
+      extends AnimatedWithChildren {
+      interpolate(
+        config: InterpolationConfigType,
+      ): AnimatedInterpolation<OutputT>;
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      __getValue: () => OutputT;
+    }
+  }
+
   // <------ REACT NATIVE WEB (0.19.0) ------>
   // Extracted from react-native-web, packages/react-native-web/src/exports/View/types.js
   type idRef = string;
@@ -388,14 +441,4 @@ declare module 'react-native' {
   interface TextStyle extends WebStyle {}
   interface ImageStyle extends WebStyle {}
   // <------ REACT NATIVE WEB (0.19.0) ------>
-
-  interface NativeModulesStatic {
-    BootSplash: BootSplashModule;
-    HybridAppModule: HybridAppModule;
-    StartupTimer: StartupTimer;
-    // RNTextInputReset: RNTextInputResetModule;
-    // RNNavBarManager: RNNavBarManagerModule;
-    // EnvironmentChecker: EnvironmentCheckerModule;
-    // ShortcutManager: ShortcutManagerModule;
-  }
 }
