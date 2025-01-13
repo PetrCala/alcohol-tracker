ca17044382bd84db89084d8d27a753190b87c774
PetrCala <cala.p@seznam.cz>
2025-01-13 17:14:03 +0100

    Rewrite the user connection context using a hook

--- src/components/UserOfflineModal.tsx
+++ src/components/UserOfflineModal.tsx
@@ -1,7 +1,8 @@
 import useThemeStyles from '@hooks/useThemeStyles';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import CONST from '@src/CONST';
 import useLocalize from '@hooks/useLocalize';
+import Text from '@components/Text';
 import SafeAreaConsumer from './SafeAreaConsumer';
 import Modal from './Modal';
 
